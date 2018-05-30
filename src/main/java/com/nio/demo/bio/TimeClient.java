2a44d92d0f67953adfa95617339d4c926dd1e5d8
shinhye218 <seeumonkey@gmail.com>
2018-05-30 13:31:43 +0800

    Initial commit

---
+++ src/main/java/com/nio/demo/bio/TimeClient.java
@@ -0,0 +1,7 @@
+package com.nio.demo.bio;
+
+/**
+ * Created by BoA on 2018/5/30.
+ */
+public class TimeClient {
+}
---
+++ src/main/java/com/nio/demo/bio/TimeServerHandler.java
@@ -0,0 +1,13 @@
+package com.nio.demo.bio;
+
+/**
+ * Created by BoA on 2018/5/30.
+ */
+public class TimeServerHandler implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+
+}
