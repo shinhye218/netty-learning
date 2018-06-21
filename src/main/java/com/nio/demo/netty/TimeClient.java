81a99dfd7b2ac153291e35c8a90cffd5860c32a1
shinhye218 <seeumonkey@gmail.com>
2018-06-21 15:50:03 +0800

    添加netty timeserver demo

---
+++ src/main/java/com/nio/demo/netty/TimeClient.java
@@ -0,0 +1,55 @@
+package com.nio.demo.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Created by BoA on 2018/6/21.
+ */
+public class TimeClient {
+
+    public void connect(int port, String host) throws Exception {
+        //配置客户端NIO线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+
+            //发起异步连接操作
+            ChannelFuture f = b.connect(host, port).sync();
+
+            //等待客户端链路关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+
+            }
+        }
+        new TimeClient().connect(port, "127.0.0.1");
+    }
+    
+}
